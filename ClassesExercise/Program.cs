c56b8d0949879d8cc203b1656432f1f072c2ffaa
hunterSherouse <huntersherouse2@gmail.com>
2021-04-12 13:52:54 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //default constructor examples
+            Car myCar = new Car();
+                            //^contructor-helps create new instance of class
+            myCar.Make = "Ford";
+            myCar.Model = "Ranger";
+            myCar.Year = 2002;
+
+            //or
+
+            Car mazda = new Car() { Make = "mazda", Model = "rx8", Year = 2012 };
+
+            //or with non-default constructor:
+
+            Car chevy = new Car("Chevy", "z-71", 2021);
+
+
+        }
+    }
+}
